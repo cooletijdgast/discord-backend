8b95b7ad6f520283f369e63ce15913dd1d34038e
Cooletijdgast <larsbrinker2000@gmail.com>
2023-04-03 23:24:23 +0200

    Small fixes

--- server.ts
+++ server.ts
@@ -1,13 +1,30 @@
-import express from "express";
+import express, { Application, Request, Response } from 'express';
 import connection from "./src/util/connection";
-import Sound from "./src/models/Sound";
 
 // Constants
-const PORT = 8080;
+const PORT = 4000;
 const HOST = "0.0.0.0";
 
 // App
 const app = express();
+
+app.use(function (_request: Request, response: Response, next) {
+  response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+  response.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  response.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  response.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+// Body parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   const all = () => connection.get("sounds").value();
